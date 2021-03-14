2e87dcae0accf60dc3d0c610e8dc1a1b5576292e
Alexander Rusak <375298388602@mail.ru>
2021-03-14 23:16:19 +0300

    create first async for people and push them in DOM

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,22 +1,77 @@
-const cards = Array.from(document.getElementsByClassName("primary-card"));
-const closeElement = Array.from(
-  document.getElementsByClassName("fa-times-circle")
-);
+import * as Handlebars from "handlebars/dist/cjs/handlebars";
+
+
 
 function setCardEvents() {
-  cards.forEach((el) => {
-    el.removeAttribute("id");
-  });
-  this.getAttribute("id") === "expand-card"
-    ? this.removeAttribute("id")
-    : this.setAttribute("id", "expand-card");
+  const element = this.closest(".primary-card");
+  element.setAttribute("id", "expand-card");
+
 }
-cards.forEach((element) => {
-  element.addEventListener("click", setCardEvents);
-});
-closeElement.forEach((close) => {
-  close.addEventListener("click", function () {
-    this.closest(".primary-card").removeEventListener("click", setCardEvents);
-    this.closest(".primary-card").removeAttribute("id");
+
+
+
+const getDataFromApi = async (category, count) => {
+  const result = [];
+  for (let i = 1; i <= (count / 10); i++) {
+    await fetch(`https://swapi.dev/api/${category}/?limit=10&page=${i}`).then(data => data.json()).then(({ results }) => {
+      result.push(...results);
+    });
+  }
+  return result;
+}
+
+const getCardTemplate = (data) => {
+  const { name, gender, birth_year, homeworld, height, mass, hair_color, eye_color, skin_color } = data;
+
+  const cardTemplate = Handlebars.compile(`<div class="primary-card">
+  <i class="far fa-times-circle"></i>
+  <h6>{{name}}</h6>
+  <div class="primary-card__info">
+    <p>Gender: {{gender}}</p>
+    <p>Birth year: {{birth_year}}</p>
+    <p>Home World: {{homeworld}}</p>
+  </div>
+  <div class="primary-card__info-additional">
+    <p>Height: {{height}}</p>
+    <p>Mass: {{mass}}</p>
+    <p>Hair color: {{hair_color}}</p>
+  </div>
+  <div class="primary-card__info-additional">
+    <p>Eye color: {{eye_color}}</p>
+    <p>Skin color: {{skin_color}}</p>
+  </div>
+  <p class="primary-card__info-label">Click to get more</p>
+</div>`);
+  return cardTemplate({ name, gender, birth_year, homeworld, height, mass, hair_color, eye_color, skin_color });
+}
+
+
+const appendTempalte = async () => {
+  const container = document.getElementById("card-section");
+  const data = await getDataFromApi("people", 10).then((results) => results);
+  const elements = [];
+  await data.forEach((card) => {
+    elements.push(getCardTemplate(card));
   });
-});
+  container.innerHTML = elements;
+
+
+  const cards = Array.from(document.getElementsByClassName("primary-card"));
+  const cardsButton = Array.from(document.getElementsByClassName("primary-card__info-label"));
+  const closeElement = Array.from(
+    document.getElementsByClassName("fa-times-circle")
+  );
+  await cardsButton.forEach((element) => {
+    element.addEventListener("click", setCardEvents);
+  });
+  await closeElement.forEach((close) => {
+    close.addEventListener("click", function () {
+      this.closest(".primary-card").removeAttribute("id");
+    });
+  });
+
+
+}
+appendTempalte();
+
+//getDataFromApi("people", 10).then((res) => { console.log(res) });
