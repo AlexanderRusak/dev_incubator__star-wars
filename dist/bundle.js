b10f2bf50182ca06fc4026e4bcc83e41f63617fa
Alexander Rusak <375298388602@mail.ru>
2021-03-20 03:00:01 +0300

    use LS to get data and use pagination query

--- dist/bundle.js
+++ dist/bundle.js
@@ -358,8 +358,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var hand
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
